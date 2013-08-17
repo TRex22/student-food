f12ae68c181fbde1119cb75014d62c90e4b08455
Daniel Levin <daniellevin2607@gmail.com>
2013-08-17 11:12:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studentfood::Application.config.secret_token = '8cb6778caee61d09b2143ffea597ede488b4c4a490bd4d6ab5ca79d0bef4c3baceb51842ad31b55d2a66dfc014b55c5d8369265bfc31656d14aa5e40fe04ae2d'
